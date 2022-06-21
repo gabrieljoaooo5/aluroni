d8f28f5db10efbe63f483ba54c4e87d9524a7342
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-21 14:32:53 -0300

    Create search engine

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,10 @@
 import styles from './Menu.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
+import Search from './Search';
+import { useState } from 'react';
 
 export default function Menu() {
+    const [search, setSearch] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -12,6 +15,10 @@ export default function Menu() {
                     The pasta house
                 </div>
             </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Menu</h3>
+                <Search search={search} setSearch={setSearch} />
+            </section>
         </main>
     )
 }
