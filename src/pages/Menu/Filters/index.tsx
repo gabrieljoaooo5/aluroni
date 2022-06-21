d06473e8641381bc3ed33a6cab1a5490f1221bc0
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-21 15:31:30 -0300

    Create filters

---
+++ src/pages/Menu/Filters/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import filters from "./filters.json";
+import styles from './Filters.module.scss';
+import classNames from 'classnames';
+
+type IFilterType = typeof filters[0];
+
+interface Props {
+  filter: number | null;
+  setFilter: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filters({ filter, setFilter }: Props) {
+  
+  function selectFilterType(filterType: IFilterType) {
+    if (filter == filterType.id) return setFilter(null);
+    return setFilter(filterType.id);
+  }
+  return (
+    <div className={styles.filters}>
+      {filters.map(filterType => (
+        <button className={classNames({
+          [styles.filters__filter]: true,
+          [styles['filters__filter--active']]: filter === filterType.id
+        })} key={filterType.id} onClick={() => selectFilterType(filterType)}>
+          {filterType.label}
+        </button>
+      ))}
+    </div>
+  )
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -2,9 +2,11 @@ import styles from './Menu.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import Search from './Search';
 import { useState } from 'react';
+import Filters from './Filters';
 
 export default function Menu() {
     const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState<number | null>(null);
     return (
         <main>
             <nav className={styles.menu}>
@@ -19,6 +21,9 @@ export default function Menu() {
                 <h3 className={styles.menu__title}>Menu</h3>
                 <Search search={search} setSearch={setSearch} />
             </section>
+            <div className={styles.menu__filters}>
+                <Filters filter={filter} setFilter={setFilter} />
+            </div>
         </main>
     )
 }
