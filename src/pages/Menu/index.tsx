5dc95b27ab8cae84bebcaa43672408ac9775baf6
Joao Gabriel <joaogabriel1598@gmail.com>
2022-06-22 08:57:49 -0300

    Create items list

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -4,6 +4,7 @@ import Search from './Search';
 import { useState } from 'react';
 import Filters from './Filters';
 import Order from './Order';
+import Items from './Items';
 
 export default function Menu() {
     const [search, setSearch] = useState("");
@@ -22,11 +23,12 @@ export default function Menu() {
             <section className={styles.menu}>
                 <h3 className={styles.menu__title}>Menu</h3>
                 <Search search={search} setSearch={setSearch} />
+                <div className={styles.menu__filters}>
+                    <Filters filter={filter} setFilter={setFilter} />
+                    <Order order={order} setOrder={setOrder} />
+                </div>
+                <Items />
             </section>
-            <div className={styles.menu__filters}>
-                <Filters filter={filter} setFilter={setFilter} />
-                <Order order={order} setOrder={setOrder} />
-            </div>
         </main>
     )
 }
